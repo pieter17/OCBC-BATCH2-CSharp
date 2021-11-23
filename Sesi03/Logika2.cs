57098046841c431f39154749ea3401d1feae9f95
Pieter Wenji <pieterjake321@gmail.com>
2021-11-23 10:16:58 +0700

    finish sesi 3

---
+++ Sesi03/Logika2.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Logika2
+{
+    public static void Main()
+    {
+        int nilai = 75;
+        if (nilai < 60)
+        {
+            Console.WriteLine("Nilai Kamu C");
+        }
+        else if (nilai < 80)
+        {
+            Console.WriteLine("Nilai kamu B");
+        }
+        else
+        {
+            Console.WriteLine("Nilai kamu A");
+        }
+    }
+}
---
+++ Sesi03/Logika3.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Logika3
+{
+    public static void Main()
+    {
+        string Username;
+        string Password;
+
+        Console.Write("Username: ");
+        Username = Console.ReadLine();
+        Console.Write("Password: ");
+        Password = Console.ReadLine();
+
+        if (Username == "ocbc" && Password == "bootcamp")
+        {
+            Console.WriteLine("Anda Berhasil login");
+        }
+        else
+        {
+            Console.WriteLine("Username atau Password anda salah");
+        }
+    }
+}
