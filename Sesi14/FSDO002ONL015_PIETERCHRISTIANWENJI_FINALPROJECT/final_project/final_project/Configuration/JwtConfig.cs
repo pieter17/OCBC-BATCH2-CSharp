1240dae0873169dbc4b46d5f1fa6eba3ab195b58
Pieter Wenji <pieterjake321@gmail.com>
2021-12-01 15:30:00 +0700

    add docs

--- Sesi14/FSDO002ONL015_PIETERCHRISTIANWENJI_FINALPROJECT/final_project/final_project/Configuration/JwtConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace final_project.Configuration
-{
-    public class JwtConfig
-    {
-        public string Secret { get; set; }
-    }
-}
--- Sesi14/FSDO002ONL015_PIETERCHRISTIANWENJI_FINALPROJECT/final_project/final_project/Models/DTO/Responses/RegistrationResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using final_project.Configuration;
-
-namespace final_project.Models.DTO.Responses
-{
-    public class RegistrationResponse:AuthResult
-    {
-        
-    }
-}
